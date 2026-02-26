0b34544f6cbcf373c77c62393c38103064bd0ccd
Jack Pitson <jackpitson@gmail.com>
2026-02-26 21:12:53 +1100

    Add home redirect

--- competition/urls.py
+++ competition/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('tip/', views.submit_tip, name='submit_tip'),
+    path("", views.home, name="home"),        # 👈 THIS LINE
+    path("signup/", views.signup, name="signup"),
+    path("tip/", views.submit_tip, name="submit_tip"),
 ]
