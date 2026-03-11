aee0566e92247f8baac28422d88cc23020b4ef5a
Jack Pitson <jackpitson@gmail.com>
2026-03-11 18:15:18 +1100

    work please

--- build.sh
+++ build.sh
@@ -7,3 +7,11 @@ python -m pip install gunicorn
 
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+pip install -r requirements.txt
+
+python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@example.com','admin123')" | python manage.py shell
